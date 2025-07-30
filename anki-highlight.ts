b9c524788bd2bc5603dddf4afa5f61646421dc5b
Luc Mcgrady <lucmcgrady@gmail.com>
2025-07-30 21:29:01 +0100

    Added: Highlight code

--- anki-highlight.ts
+++ anki-highlight.ts
@@ -22,7 +22,24 @@ import * as _ from "lodash-es"
     const card_info_req = await fetch("http://127.0.0.1:8765", { body: JSON.stringify(cards_info_req), method: "POST", headers: { "Content-Type": "application/json" } })
     const card_info = (await card_info_req.json())
 
-    const card_card_info = _.groupBy(card_info, ["fields", match_field, "value"])
+    const card_card_info = _.groupBy(card_info, card=>card.fields[match_field]?.value)
 
-    console.log(card_card_info)
+    console.log({card_info,card_card_info})
+
+    const colorMap = _.mapValues(card_card_info, (cards) => {
+      return "red"
+    })
+
+    // Escape and join all keys into a single regex alternation
+    const escapedKeys = Object.keys(colorMap).map(s =>
+      s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    );
+    const combinedRegex = new RegExp(`(${escapedKeys.join("|")})`, "g");
+
+    // One-pass replace
+    document.body.innerHTML = document.body.innerHTML.replace(combinedRegex, (match) => {
+      const color = colorMap[match];
+      return `<span style="background-color: ${color};">${match}</span>`;
+    });
+    
   })()
